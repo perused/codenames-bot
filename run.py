ff9b02a5583c4e41789ffddcc9f7a7bb170bb45a
Sam <sjac6722@uni.sydney.edu.au>
2021-07-14 11:17:22 +1000

    Cleaned up spymaster and player turns. Ready to fit the guesses into the game

--- run.py
+++ run.py
@@ -29,12 +29,13 @@ class Bot:
         print("\nThank you, card accepted.")
         time.sleep(1)
         os.system("clear")
-        self.all_cards = self.bot_cards + self.not_bot_cards + self.neutral_cards
+        self.all_cards = self.bot_cards + self.not_bot_cards + self.neutral_cards + [self.black_card]
 
     def populate_board_player(self):
         self.request_cards("on the board", self.all_cards)
 
     def print_board(self):
+        os.system("clear")
         print(f"Role: {'Spymaster' if self.role == 's' else 'Player'}")
         print("Cards remaining:")
         for i, card in enumerate(self.all_cards):
@@ -55,41 +56,60 @@ class Bot:
         print("\nThank you, cards accepted.")
         time.sleep(1)
         os.system("clear")
+
+    def remove_append(self, card, ls, removed):
+        try:
+            ls.remove(card)
+            removed.add(card)
+        except:
+            pass
+        return
                 
     def remove_cards_spymaster(self, cards):
+        removed = set()
         for card in cards:
             card = card.strip().lower()
-            removed = False
-            self.all_cards.remove(card)
-            self.bot_cards.remove(card)
-            self.not_bot_cards.remove(card)
+            self.remove_append(card, self.all_cards, removed)
+            self.remove_append(card, self.bot_cards, removed)
+            self.remove_append(card, self.not_bot_cards, removed)
             if card == self.black_card:
-                print("Removing black card ends the game, are you sure?")
+                print("Removing black card ends the game, are you sure?\n")
                 response = input("Enter 'y' or 'n': ").lower()
                 if response == "y":
-                    print("Game over.")
+                    print("\nGame over.")
                     sys.exit()
                 else:
                     print("Skipping.")
                     continue
-
+        print(f"Successfully removed {'no cards :/' if len(removed) == 0 else ', '.join(removed)}")
+        time.sleep(2)
 
     def remove_cards_player(self, cards):
-        pass
+        removed = []
+        for card in cards:
+            card = card.strip().lower()
+            self.remove_append(card, self.all_cards, removed)
+        print(f"Successfully removed {'no cards :/' if len(removed) == 0 else ', '.join(removed)}")
+        time.sleep(2)
     
     def play(self):
         second_option = "give a bot a clue and a number to guess some cards" if self.role == "p" else "request a clue from the bot"
         while True:
             self.print_board()
-            print(f"Enter one of the following:\n'r' to remove cards that have been guessed.\n{second_option}\n'q' to quit\n")
+            print(f"Enter one of the following:\n'r' to remove cards that have been guessed.\n'c' to {second_option}\n'q' to quit\n")
             entry = input("Enter here: ")
+            print()
             if entry == "r":
-                print("Please enter all the cards that have been guessed, space separated: ")
+                print("Please enter all the cards to be removed.\n")
                 cards = input("Enter here: ").split()
+                print()
                 if self.role == "s":
                     self.remove_cards_spymaster(cards)
                 else:
                     self.remove_cards_player(cards)
+                if len(self.all_cards) == 0:
+                    print("No more cards remaining, game over.")
+                    sys.exit()
             elif entry == "c":
                 if self.role == "s":
                     self.get_best_spymaster_clue()
@@ -105,12 +125,14 @@ class Bot:
                 print("Exiting.")
                 sys.exit()        
 
-    def get_best_spymaster_clue(self, team):
+    def get_best_spymaster_clue(self):
         # the best clue should take into account our teams similarity - other team similarity 
-        pass
+        print("Bot is thinking of a clue...")
+        time.sleep(5)
 
     def get_best_player_guess(self, clue, number):
-        pass
+        print("Bot is thinking about what to guess...")
+        time.sleep(5)
 
 # find out whether bot is playing as spymaster or player
 def get_role():
@@ -125,7 +147,7 @@ def get_role():
             print("Invalid input. Please enter 's' for Spymaster or 'p' for player.")
     print()
     os.system("clear")
-    print(f'{"Spymaster" if role == "s" else "Player"} selected!')
+    print(f'{"Spymaster" if role == "s" else "Player"} selected!\n')
     time.sleep(1)
     return role
 
